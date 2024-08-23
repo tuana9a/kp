709cb022d679450dbc1b13550d4e9babc4eb30bb
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-08-23 13:21:10 +0000

    feat: add longhorn requirements

--- examples/userdata/worker-1.30.sh
+++ examples/userdata/worker-1.30.sh
@@ -47,3 +47,8 @@ apt install -y kubelet='1.30.2-*' kubeadm='1.30.2-*' kubectl='1.30.2-*'
 
 # https://github.com/kubernetes-sigs/nfs-subdir-external-provisioner
 apt install -y nfs-common
+
+# https://longhorn.io/docs/1.7.0/deploy/install/#installation-requirements
+apt-get install -y open-iscsi
+systemctl start iscsid
+systemctl enable iscsid
